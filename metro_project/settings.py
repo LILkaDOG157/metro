617f5b3b67fa68afc5b9b490c3f84f33ac6b4967
LILkaDOG157 <yaemmars6446@gmail.com>
2025-05-15 19:31:54 +0300

    Добавлены станции Большой кольцевой линии метро

--- metro_project/settings.py
+++ metro_project/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'metro_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'metro_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
